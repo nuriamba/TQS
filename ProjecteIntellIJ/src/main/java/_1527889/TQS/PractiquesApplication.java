1578b333ef0a0f0a6e2ed4af50736a2464d226c8
nuriamba <65227508+nuriamba@users.noreply.github.com>
2021-10-05 13:56:38 +0200

    Creació Projecte IntellIJ

---
+++ ProjecteIntellIJ/src/main/java/_1527889/TQS/PractiquesApplication.java
@@ -0,0 +1,13 @@
+package _1527889.TQS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiquesApplication.class, args);
+	}
+
+}
---
+++ ProjecteIntellIJ/src/test/java/_1527889/TQS/PractiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package _1527889.TQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
