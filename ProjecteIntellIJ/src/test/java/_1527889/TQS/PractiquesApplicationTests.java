c68845b6c7e148d2b53c583fedbc737550cf5c69
maymac00 <arnau.mayoral10@gmail.com>
2021-10-19 10:46:30 +0200

    Ideas de tests

--- ProjecteIntellIJ/src/test/java/_1527889/TQS/PractiquesApplicationTests.java
+++ ProjecteIntellIJ/src/test/java/_1527889/TQS/PractiquesApplicationTests.java
@@ -21,11 +21,25 @@ class PractiquesApplicationTests {
 	// Test del constructor, mira que las medidas sean las correctas
 	@ParameterizedTest
 	@ValueSource (ints = {n,m})
-	void creaTauler(int[] dim){
+	void TestCreaTauler(int[] dim){
 		Tauler t = new Tauler(n, m);
 
 		assertEquals(n, t.getN());
 		assertEquals(m, t.getM());
+	}
+
+	//En este testearemos la generacion de una matriz nxm con 1 i 0 distribuidos aleatoriamente.
+	// Aquí la idea és hacer un mock de la libreria random. Para "forzar" a que el aleatorio genere una matriz
+	// que nosotros queramos. Luego habra que hacer un assert entre la matriz que se genera i la que nos deberia dar (que es lo que hayamos puesto en el mock)
+	@Test
+	void TestGenerateBombs() {
+
+	}
+
+	// Este test sera sencillito. Habra que generar una matriz con la funcion mencionada anteriormente, i assignar-la a un objeto Tauler. Luego hacer assert de que se haya asignado bien
+	@Test
+	void TestSetBombs(){
+
 	}
 
 	// Test que recibe una matriz NxM (tablero) i que dado una casilla devuelve el numero de bombas de alrededor.
