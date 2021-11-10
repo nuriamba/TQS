d61fea4cc72a68b1c7025b1838390c4d0cdb1d09
maymac00 <arnau.mayoral10@gmail.com>
2021-11-10 17:17:44 +0100

    test TestGetRandomMatrix

--- ProjecteIntellIJ/src/test/java/_1527889/TQS/RandomTests.java
+++ ProjecteIntellIJ/src/test/java/_1527889/TQS/RandomTests.java
@@ -2,12 +2,14 @@ package _1527889.TQS;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomTests {
 
+    //Test del mock
     @ParameterizedTest
     @MethodSource(value = "_1527889.TQS.ParamProvider#matriusSimples")
     void TestSetGetReturnMatrix(boolean[][] mat) {
@@ -16,5 +18,14 @@ public class RandomTests {
         //n i m són arbitraris ja que no canviaran la matriu que retornara el mock
         assertEquals(mock.generateRandomMatrix(2,3), mat);
     }
+    //Test del Random real
+    @ParameterizedTest
+    @CsvSource(value = {"8,8","16,16","16,30","1,3","100,100","400,400"})
+    void TestGetRandomMatrix(int n, int m) {
+        Random ran = new Random();
 
+        boolean[][] mat = ran.generateRandomMatrix(n,m);
+        assertEquals(mat.length, n);
+        assertEquals(mat[0].length, m);
+    }
 }
