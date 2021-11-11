fb1ad421c405864e95ba4cd3487c4e34131fe2db
maymac00 <arnau.mayoral10@gmail.com>
2021-11-11 18:24:15 +0100

    Afegit pseudocodi de la funció recursiva, sense el pseudocodi es dificil de veure com testejar.
    Afegides macros que representen el resultat d'obrir casella. (Entenem que això no requereix ni admet testeig

--- ProjecteIntellIJ/src/main/java/_1527889/TQS/Vista.java
+++ ProjecteIntellIJ/src/main/java/_1527889/TQS/Vista.java
@@ -2,7 +2,10 @@ package _1527889.TQS;
 
 public class Vista implements IVista{
     Tauler bombas;
-
+    public static final int CASELLA_OBERTA = 0;
+    public static final int GAME_OVER = -1;
+    public static final int SUCCESS = 1;
+    char[][] tauler;
     @Override
     public void print() {
 
@@ -19,8 +22,21 @@ public class Vista implements IVista{
     }
 
     @Override
-    public void obre(int i, int j) {
-
+    public int obre(int i, int j) {
+        /*PSEUDOCODI
+        * Si (i,j) és casella tancada
+        *   n = Tauler.open(i,j)
+        *   Si n == 0
+        *       setCasella(i,j, '0')
+        *       this.obre(i+-1, j+-1)
+        *   Si n == -1
+        *       return CODI_GAME_OVER
+        *   Sino
+        *       setCasella(i,j, n)
+        * Sino
+        *   return CODI_CASELLA_OCUPADA
+        * */
+        return 0;
     }
 
 }
