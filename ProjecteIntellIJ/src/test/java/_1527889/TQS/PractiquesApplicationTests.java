c62552acabf6586d05f536a87b7fee02a946a40f
maymac00 <arnau.mayoral10@gmail.com>
2021-11-11 11:59:39 +0100

    afegides anotacions als primers testos, i afegit un nou test del open que correspon al cas de un taulell de 1x1

--- ProjecteIntellIJ/src/test/java/_1527889/TQS/PractiquesApplicationTests.java
+++ ProjecteIntellIJ/src/test/java/_1527889/TQS/PractiquesApplicationTests.java
@@ -4,6 +4,7 @@ package _1527889.TQS;
 import org.javatuples.Quartet;
 import org.javatuples.Triplet;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,7 +18,8 @@ class PractiquesApplicationTests {
 	// Test raco dels tauler
 
 	// Test que recibe una matriz NxM (tablero) i que dado una casilla devuelve el numero de bombas de alrededor.
-	// Test caixa negra
+	// Farem 3 testos corresponents a les particions equivalents del: Cas base, cantonades, parets
+	// Test caixa negra , partició equivalent: Cas base
 	@ParameterizedTest
 	@MethodSource(value = "_1527889.TQS.ParamProvider#taulersSimples")
 	void TestBombasAlrededorSimple(Quartet<Tauler, Integer, Integer, Integer> q) {
@@ -25,7 +27,7 @@ class PractiquesApplicationTests {
 		int r = t.open(q.getValue1(),q.getValue2());
 		assertEquals(q.getValue3(), r);
 	}
-	// Test caixa negra
+	// Test caixa negra, partició equivalent: Cantonades
 	@ParameterizedTest
 	@MethodSource(value = "_1527889.TQS.ParamProvider#taulersEsquinas")
 	void TestBombasAlrededorEsquinas(Quartet<Tauler, Integer, Integer, Integer> q) {
@@ -33,15 +35,8 @@ class PractiquesApplicationTests {
 		int r = t.open(q.getValue1(),q.getValue2());
 		assertEquals(q.getValue3(), r);
 	}
-	// Test caixa negra
-	@ParameterizedTest
-	@MethodSource(value = "_1527889.TQS.ParamProvider#taulersEstrechos")
-	void TestBombasAlrededorEstrechos(Quartet<Tauler, Integer, Integer, Integer> q) {
-		Tauler t = q.getValue0();
-		int r = t.open(q.getValue1(),q.getValue2());
-		assertEquals(q.getValue3(), r);
-	}
-	// Test caixa negra
+
+	// Test caixa negra, partició equivalent: parets
 	@ParameterizedTest
 	@MethodSource(value = "_1527889.TQS.ParamProvider#taulersParedes")
 	void TestBombasAlrededorParedes(Quartet<Tauler, Integer, Integer, Integer> q) {
@@ -49,6 +44,32 @@ class PractiquesApplicationTests {
 		int r = t.open(q.getValue1(),q.getValue2());
 		assertEquals(q.getValue3(), r);
 	}
+
+	// Test caixa negra, cas especial taulers de una sola fila o columna
+	@ParameterizedTest
+	@MethodSource(value = "_1527889.TQS.ParamProvider#taulersEstrechos")
+	void TestBombasAlrededorEstrechos(Quartet<Tauler, Integer, Integer, Integer> q) {
+		Tauler t = q.getValue0();
+		int r = t.open(q.getValue1(),q.getValue2());
+		assertEquals(q.getValue3(), r);
+	}
+
+	// Test caixa negra, cas especial tauler 1x1
+	@Test
+	void TestBombasAlrededor1x1() {
+
+		boolean[][] mat = {{false}};
+
+		Tauler t = new Tauler(1,1);
+		RandomMock mock = new RandomMock();
+		mock.setReturnMatrix(mat);
+		t.setRand(mock);
+		t.generateBombs();
+
+		int r = t.open(0,0);
+		assertEquals(0, r);
+	}
+
 
 	// Test PairWise. Obrir totes les combinacions de variables d'entrada
 	@ParameterizedTest
