57b05257ba2f4896c8b5e806b5dc9141eff15879
maymac00 <arnau.mayoral10@gmail.com>
2021-11-11 11:15:46 +0100

    implementació metode Random amb dificultat escollida. Pasa tots els testos a la primera

--- ProjecteIntellIJ/src/main/java/_1527889/TQS/Random.java
+++ ProjecteIntellIJ/src/main/java/_1527889/TQS/Random.java
@@ -1,5 +1,7 @@
 package _1527889.TQS;
 
+import java.util.SplittableRandom;
+
 public class Random implements IRandom{
     @Override
     public boolean[][] generateRandomMatrix(int n, int m) {
@@ -15,6 +17,30 @@ public class Random implements IRandom{
 
     @Override
     public boolean[][] generateRandomMatrix(int n, int m, String dificultad) {
-        return new boolean[0][];
+         SplittableRandom rd = new SplittableRandom();
+
+        boolean[][] mat = new boolean[n][m];
+        if(dificultad == "facil"){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    mat[i][j]= rd.nextInt(4) == 0;
+                }
+            }
+        }
+        else if(dificultad == "mitja"){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    mat[i][j]= rd.nextInt(3) == 0;
+                }
+            }
+        }
+        else if(dificultad == "dificil"){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    mat[i][j]= rd.nextInt(2) == 0;
+                }
+            }
+        }
+        return mat;
     }
 }
